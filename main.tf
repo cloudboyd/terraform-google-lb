5f69ce2922581959959b2c24a6140c77c16b2f9d
Likhit Siriarayapan <likhit@xponential.asia>
2024-07-08 14:27:10 +0700

    fix(purpose): Assign purpose when using internal address type

--- main.tf
+++ main.tf
@@ -52,6 +52,7 @@ resource "google_compute_target_https_proxy" "lb_target_https_proxy" {
 resource "google_compute_global_address" "ip" {
   name         = var.name
   address_type = var.address_type
+  purpose      = var.address_type == "INTERNAL" ? "PRIVATE_SERVICE_CONNECT" : null
 }
 
 resource "google_compute_url_map" "http_to_https_redirect" {
